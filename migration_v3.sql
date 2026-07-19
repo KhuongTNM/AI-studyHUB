b9b7b48230be43b08823ac4cdbc6ffa73c9f4169
Zero <baoquan2604@gmail.com>
2026-07-19 14:29:12 +0700

    chore: Increase name column length in migration_v3.sql to allow longer deleted name suffixes

--- migration_v3.sql
+++ migration_v3.sql
@@ -1,4 +1,5 @@
--- Migration V3: Add daily_ai_chat_limit and max_flashcards to subscription_plans
+-- Migration V3: Add daily_ai_chat_limit and max_flashcards to subscription_plans, increase name length
+ALTER TABLE payment.subscription_plans ALTER COLUMN name TYPE VARCHAR(100);
 ALTER TABLE payment.subscription_plans ADD COLUMN IF NOT EXISTS daily_ai_chat_limit INTEGER NOT NULL DEFAULT 5;
 ALTER TABLE payment.subscription_plans ADD COLUMN IF NOT EXISTS max_flashcards INTEGER NOT NULL DEFAULT 5;
 
